7cdb9d944cac9eff41ce19e3be6aa2ab7a1e62d3
ngaptph19327 <ngaptph19327@fpt.edu.vn>
2023-05-28 07:49:04 +0700

    tao commponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ListProductComponent } from './list-product/list-product.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProductComponent,
+    ListProductComponent,
+    UpdateProductComponent
   ],
   imports: [
     BrowserModule,
